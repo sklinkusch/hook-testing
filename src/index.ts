6c860e199e19281478a7f31dc21bb7a51ce64a7d
Stefan Klinkusch <stefan.klinkusch@googlemail.com>
2024-02-10 10:01:18 +0100

    remove all linting errors

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 import { useState } from 'react';
 
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
 export function useForm(formData: any) {
   const [values, setValues] = useState(formData);
 
