66c61317036d6bb83d2d376be6c51028cf2da698
Stefan Klinkusch <stefan.klinkusch@googlemail.com>
2024-02-26 21:36:00 +0100

    remove localStorage from useTheme hook

--- src/index.ts
+++ src/index.ts
@@ -8,19 +8,13 @@ const getTheme = (displayMode = 1) => {
   return displayMode === 1 ? 'dark' : 'light';
 };
 
-type LocalStorage = {
-  getItem: (key: string) => string;
-  setItem: (key: string, value: string) => void;
-  removeItem: (key: string) => void;
-  clear: () => void;
-};
-
-export const useTheme = (localStorage: LocalStorage) => {
+export const useTheme = () => {
   const [displayMode] = useLocalStorage('sedna.cloud.frontend.displayMode', 1);
+  const setStoreTheme = useLocalStorage<'dark' | 'light'>('RaStore.theme', 'dark')[1];
   const [defaultTheme, setDefaultTheme] = useState<AdminProps['defaultTheme']>(getTheme(displayMode));
 
   useLayoutEffect(() => {
-    localStorage.setItem('RaStore.theme', getTheme(displayMode));
+    setStoreTheme(getTheme(displayMode));
     setDefaultTheme(getTheme(displayMode));
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [displayMode]);
